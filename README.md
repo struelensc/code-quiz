ce0f345c61cefd9e2be3b72a96451fa3a141e904
Courtney Struelens <courtneystruelens@hotmail.com>
2022-03-19 23:51:19 -0700

    Added js functionality and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # code-quiz
+
+This is a single page application that functions as a coding quiz challenge.
+
+## Purpose
+
+The purpose of this project was to practice using web APIs in javascript to make updates to the DOM.
+
+## Goal
+
+The goal of this project was to provide a user with a single page application that acted as a coding quiz. At the end of the quiz they could choose to store their score on the locally hosted highscore page.
+
+## Deployment
+
+See below for a screenshot of the application as well as a link to the deployed web page.
+
+https://struelensc.github.io/code-quiz/
+
+![code quiz screenshot](code-quiz_index.html.png)
+
+## Some Thoughts
+
+In the function `questionResult()`, lines 113-131, there is some repeated code for displaying a message to the user if they chose the correct or wrong answer. I would like optimize this by creating a separate function for displaying this message to the user.
+
+In the `generateHighscores()`, lines 197-212, button that clears local storage and by doing so, clears the highscore board I created a while loop to remove all the children of the highscore table. This also ends up removing the headers for this table as well. To fix I created new elements and appended them to the DOM. In future, I would like to find a better way to handle this so it doesn't remove the labels.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,89 @@
 // Timer handles
 var timeEl = document.querySelector(".timer");
+var timerInterval;
 var secondsLeft = 75;
 timeEl.textContent = secondsLeft;
 
-// Document handles
+// Page specific handles
 var main = document.querySelector("main");
 var titlePage = document.querySelector("#titlePage");
 var quizPage = document.querySelector("#quizPage");
 var resultsPage = document.querySelector("#resultsPage");
-var highscores = document.querySelector("#highscores");
-var pages = [titlePage, quizPage, resultsPage, highscores];
+var highscoresPage = document.querySelector("#highscores");
+var pages = [titlePage, quizPage, resultsPage, highscoresPage];
+
+// Document handles
 var correctAnswers = 0;
 var submitEL = main.querySelector("#submit");
 var score = resultsPage.querySelector("#score");
+var highscoresEL = document.querySelector("#highscoresEL");
+var highscoresTable = main.querySelector("#highscoreList");
+var goBack = main.querySelector("#goBack");
+var clearHS = main.querySelector("#clearHS");
 
-// Quiz question objects
+// Quiz question objects and variables
 var quizQuestion1 = {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     correctAnswerIndex: 3,
     answers: ["Javascript", "Terminal/Bash", "For Loops", "console.log"]
-}
+};
 
 var quizQuestion2 = {
     question: "What HTML tag is used to define a list item?:",
     correctAnswerIndex: 0,
     answers: ["<li>", "<s>", "<u>", "<ul>"]
-}
+};
 
 var quizQuestion3 = {
     question: "What is a JavaScript element that represents either TRUE or FALSE values?",
     correctAnswerIndex: 2,
     answers: ["RegExp", "Condition", "Boolean", "Event"]
-}
+};
 
 var quizQuestion4 = {
     question: "What is the name of CSS design that calls for fluid and adaptable elements based on the device resolution or size?",
     correctAnswerIndex: 3,
     answers: ["Cascading", "Evolution", "Shifting", "Responsive"]
-}
+};
 
 var quizQuestion5 = {
     question: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
     correctAnswerIndex: 1,
     answers: ["Else Loop", "For Loops", "Conditional Loop", "While Loop"]
-}
+};
 
 var questionArr = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5];
 var questionArrState = 0;
 
 // Generates title page on load and adds event listener for starting the game for the start game button
-window.addEventListener("load", function() {
-for (let i = 0; i < pages.length; i++) {
-        var state = pages[i].getAttribute("data-state")
-        
-        if (state === "none"){
-            pages[i].style.display = "none";
-        }
-    }
+window.addEventListener("load", generateTitlePage);
 
+function generateTitlePage() {
+    quizPage.style.display = "none";
+    resultsPage.style.display = "none";
+    highscoresPage.style.display = "none";
+    titlePage.style.display = "block"
+
+    highscoresEL.addEventListener("click", generateHighscores);
 
     // Starts game and timer
     startGame.addEventListener("click", function () {    
         var titlePage = main.querySelector("#titlePage");
         titlePage.style.display = "none";
 
-        var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-    
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            // generate results page
-        }
-    
+        timerInterval = setInterval(function() {
+            secondsLeft--;
+            timeEl.textContent = secondsLeft;
+        
+            if(secondsLeft === 0) {
+                clearInterval(timerInterval);
+                generateResults(correctAnswers);
+            };
         }, 1000);
 
         generateQuizPage(questionArrState);
     });
-});
+};
 
 // Generates looping quiz questions
 function generateQuizPage(index) {
@@ -95,9 +101,10 @@ function generateQuizPage(index) {
         questionAnswers.appendChild(listItem);
 
         listItem.addEventListener("click", questionResult);
-    }
-}
+    };
+};
 
+// Handles keeping track of score
 function questionResult (event){
     var element = event.target;
     var i = element.getAttribute("id");
@@ -107,39 +114,128 @@ function questionResult (event){
         // If answer is correct
         if (i == questionArr[questionArrState].correctAnswerIndex){
             result.textContent = "Correct!";
-            correctAnswers = correctAnswers + 1;
+            correctAnswers++;
         // If answer is wrong
         } else {
             secondsLeft = secondsLeft - 10;
             result.textContent = "Wrong!";
-        }
+        };
     // Terminates if all questions have been answered
     } else {
-        console.log("end of game");
+        if (i == questionArr[questionArrState].correctAnswerIndex){
+            result.textContent = "Correct!";
+            correctAnswers++;
+        } else {
+            secondsLeft = secondsLeft - 10;
+            result.textContent = "Wrong!";
+        }
+        clearInterval(timerInterval);
         generateResults(correctAnswers);
         return;
-    }
+    };
 
     quizPage.style.display = "none";
     quizPage.querySelector("ol").innerHTML = "";
 
     questionArrState++;
     generateQuizPage(questionArrState);
-}
+};
 
+// Generates the results page and form for adding to the highscores
 function generateResults() {
     quizPage.style.display = "none";
     resultsPage.style.display = "block";
 
-    score.textContent = correctAnswers * 20;
-    submitEL.addEventListener("click", formHandler);
-    localStorage.setItem("score", score);
-}
+    var finalScore = correctAnswers * 20;
+    score.textContent = finalScore;
 
-function formHandler(event) {
-    event.preventDefault();
-    var initialsEl = main.querySelector("#initials");
+    submitEL.addEventListener("click", (event)=> {
+        event.preventDefault();
+    
+        var initialsEl = main.querySelector("#initials");
+        var initials = initialsEl.value;
 
-    var initials = initialsEl.value;
-    localStorage.setItem("initials", initials);
-}
+        // Validates that the user entered in something
+        if (initials === ""){
+            return;
+        } else {
+            var userInfo = {initials, finalScore};    
+            var highscores = [];  
+    
+            if (!localStorage.getItem("highscores")){
+                highscores.push(userInfo);
+                localStorage.setItem("highscores", JSON.stringify(highscores));
+            } else {
+                var existingHighscores = localStorage.getItem("highscores");
+    
+                highscores = JSON.parse(existingHighscores);
+                highscores.push(userInfo);
+    
+                localStorage.setItem("highscores", JSON.stringify(highscores));
+            };
+            
+            generateHighscores();
+        };
+    });
+};
+
+// Generated highscore page
+function generateHighscores() {
+    titlePage.style.display = "none";
+    quizPage.style.display = "none";
+    resultsPage.style.display = "none";
+
+    // Button for Go Back
+    goBack.addEventListener("click", function() {
+        location.reload();
+    })
+
+    // Button for Clear Highscores
+    clearHS.addEventListener("click", function() {
+        localStorage.clear();
+
+        while (highscoresTable.lastChild) {
+            highscoresTable.removeChild(highscoresTable.lastChild);
+        }
+
+        var headers = document.createElement("tr");
+        var headerPlayer = document.createElement("th");
+        headerPlayer.setAttribute("class", "header");
+        var headerScore = document.createElement("th");
+        headerScore.setAttribute("class", "header");
+
+        headerPlayer.textContent = "Player";
+        headerScore.textContent = "Score";
+        headers.appendChild(headerPlayer);
+        headers.appendChild(headerScore);
+
+        highscoresTable.appendChild(headers);
+    })
+
+    // Checks to see if page is already loaded so it doesn't display local storage information again
+    if (highscoresPage.style.display === "none") {
+        highscoresPage.style.display = "block";
+        var scoreList = [];
+        var existingHighscores = localStorage.getItem("highscores");
+        scoreList = JSON.parse(existingHighscores);
+
+        if (!localStorage.getItem("highscores")) {
+            return;
+        } else {
+            for (let i = 0; i < scoreList.length; i++) {
+                var userLine = document.createElement("tr");
+                var userName = document.createElement("th");
+                var userScore = document.createElement("th");
+    
+                userName.textContent = scoreList[i].initials;
+                userScore.textContent = scoreList[i].finalScore;
+    
+                userLine.appendChild(userName);
+                userLine.appendChild(userScore);
+                highscoresTable.appendChild(userLine);
+            };
+        }
+    } else {
+        return;
+    }
+};
